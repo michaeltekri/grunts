303ac1da739aafb0bb4137d86b274656abd41230
michaeltekri <michaeltekri84@gmail.com>
2025-05-05 13:34:35 +0300

    later commit

--- app/src/main/java/com/tekri/grunts/ui/screens/people/PictureScreen.kt
+++ app/src/main/java/com/tekri/grunts/ui/screens/people/PictureScreen.kt
@@ -1,17 +1,32 @@
 package com.tekri.grunts.ui.screens.people
 
+import android.R.attr.contentDescription
+import android.R.attr.onClick
+import android.content.res.Resources
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -27,13 +42,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.tekri.grunts.R
+import com.tekri.grunts.ui.theme.Typography
+import com.tekri.grunts.ui.theme.grey
 import com.tekri.grunts.ui.theme.oceanblue
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -118,12 +143,53 @@ fun PictureScreen(navController: NavController){
                     .padding(paddingValues)
                     .fillMaxSize()
             ) {
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp),
 
 
-                //Main Contents of the page
-                Text(text = "Welcome to Homescreen Screen", fontSize = 20.sp)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text("This is where the main content goes.")
+                    ) {
+                    Column (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+
+                    ){
+                        Image(
+                            painter = painterResource(R.drawable.profile),
+                            contentDescription = "profile",
+                            modifier = Modifier.size(100.dp)
+                                .clip(androidx.compose.foundation.shape.CircleShape), // Clip to Circle
+                            contentScale = ContentScale.Crop, // Ensures image is cropped to fit the circle
+                            alignment = Alignment.Center
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.camera),
+                            contentDescription = "profile",
+                           modifier = Modifier.clickable {  }.size(30.dp),
+                            alignment = Alignment.Center
+                        )
+                        IconButton(onClick = onClick) {
+                            Image(
+                                painter = painterResource(id = R.drawable.camera),
+                                contentDescription = "Favorite",
+                                contentScale = ContentScale.Fit,
+                                modifier = Modifier.semantics {
+                                    contentDescription = "Favorite Button"
+                                }
+                            )
+                        }
+
+                        IconButton(onClick = {}, modifier = Modifier.align(alignment = Alignment.Start)) {Icon(Icons.Default.AddCircle, contentDescription = "Profile")}
+
+
+
+                    }
+                }
+
+
+
+
 
 
 
